ba4d870df8734d9e8bd9dac04e21a75a2c560801
RadmirShkred <psitecton@gmail.com>
2021-10-13 21:02:49 +0300

    feat: add decision st-remove-from-list

--- src/st-remove-from-list.js
+++ src/st-remove-from-list.js
@@ -1,4 +1,4 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const {NotImplementedError} = require('../extensions/index.js');
 
 /**
  * Given a singly linked list of integers l and an integer k,
@@ -19,7 +19,11 @@ const { NotImplementedError } = require('../extensions/index.js');
  * }
  */
 
-module.exports = function removeKFromList(/* l, k */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function removeKFromList(l, k) {
+  const res = [];
+  while(l){
+    if(l.value !== k){ res.push(l.value);}
+    l = l.next;
+  }
+  return res.reduceRight((next, value) => ({value, next}), null);
 }
